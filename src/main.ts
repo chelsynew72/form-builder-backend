a1a419cb27b4f07e780a25d36ac9c2c37be484e4
Chelsy New <chelsynew72@gmail.com>
2025-12-09 11:34:03 +0100

    deployemt

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'https://form-builder-client-xi.vercel.app/',
+    origin: 'https://form-builder-client-xi.vercel.app',
     credentials: true,
   });
 
